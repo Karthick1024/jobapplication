e3101519a7c080faad21228fa791c42d378294ab
karthick <karthickcs10124@gmail.com>
2025-01-30 21:09:45 +0530

    working

--- server.js
+++ server.js
@@ -1,9 +1,2 @@
 console.log("Server is working.....");
 
-import { value } from "./testdefault.js";
-import somevalue from "./testname.js";
-
-console.log(value);
-
-console.log(somevalue);
-jjkkjjk
