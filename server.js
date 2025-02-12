448efb21d61f728db497a86177f9aec09a42aa70
karthick <karthickcs10124@gmail.com>
2025-02-12 10:18:27 +0530

    backend

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
-console.log("Server is working.....");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+import morgan from "morgan";
 
+if (process.env.NODE_Env === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.post("/", (req, res) => {
+  res.json({ message: "Data received", data: req.body });
+});
+
+app.listen(5000, () => {
+  console.log("server is running");
+});
